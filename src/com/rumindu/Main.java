cbcb4f52739b987afc78e880a0423e90ac99d749
Rumindu De Silva <108813657+Rumindu@users.noreply.github.com>
2024-07-11 09:26:58 +0530

    toString()

--- src/com/rumindu/Main.java
+++ src/com/rumindu/Main.java
@@ -1,10 +1,10 @@
 package com.rumindu;
-//Main.java
+//main.java
 public class Main {
     public static void main(String[] args) {
         var box1= new TextBox();
         var box2= new TextBox();
-
-        System.out.println(box1.equals(box2));//false
+        System.out.println(box1.toString());
+        //com.rumindu.TextBox@eed1f14
     }
 }
