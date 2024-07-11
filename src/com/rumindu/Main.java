fe1f02400533873f8e99f18f136d8bd0193f77dc
Rumindu De Silva <108813657+Rumindu@users.noreply.github.com>
2024-07-11 09:04:09 +0530

    equals() methods comparing 2 variables with the same referencing object

--- src/com/rumindu/Main.java
+++ src/com/rumindu/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         var box1= new TextBox();
         var box2= box1;
-        System.out.println(box1.hashCode());//250421012
-        System.out.println(box2.hashCode());//250421012
+
+        System.out.println(box1.equals(box2));//true
     }
 }
