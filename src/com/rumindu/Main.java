c2d4fc2bbaf74e3caf0f092c53f3d3bad3ae6478
Rumindu De Silva <108813657+Rumindu@users.noreply.github.com>
2024-07-14 14:25:47 +0530

    decaling a method called `show` to called `toString()` methods of `UIControl` class

--- src/com/rumindu/Main.java
+++ src/com/rumindu/Main.java
@@ -1,7 +1,13 @@
 package com.rumindu;
-
+//main.java
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        var control = new UIControl(true);
+        show(control);
+    }
+    //passing UIControl object as a parameter.
+    public static void show(UIControl control){
+        //call toString() method of UIControl class
+        System.out.println(control);//com.rumindu.UIControl@1b28cdfa
     }
 }
