4c5a5160f7798d5c67ece724bee32e8f14cfca06
Rumindu De Silva <108813657+Rumindu@users.noreply.github.com>
2024-07-14 16:00:57 +0530

    Problem of downcasting implementation and solution

--- src/com/rumindu/Main.java
+++ src/com/rumindu/Main.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args) {
         var control = new UIControl(true);
         var textBox = new TextBox();
-        show(textBox);
+        show(control);
     }
     public static void show(Object control){
-        //downcasting
-        var textBox = (TextBox)control;
-        textBox.setText("Hello World");
-
-        System.out.println(control);//output: "Hello World"
+        //insteanceof operator return true if the object is an instance of the specified type
+        if(control instanceof TextBox){
+            var textBox = (TextBox)control;
+            textBox.setText("Hello World");
+        }
+        System.out.println(control);
     }
 }
