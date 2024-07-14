8d5231a30532244a80489338833c254de5d127c7
Rumindu De Silva <108813657+Rumindu@users.noreply.github.com>
2024-07-14 15:46:42 +0530

    Downcasting

--- src/com/rumindu/Main.java
+++ src/com/rumindu/Main.java
@@ -4,14 +4,13 @@ public class Main {
     public static void main(String[] args) {
         var control = new UIControl(true);
         var textBox = new TextBox();
-        //passing textBox object.
         show(textBox);
     }
-    //casting TextBox class textBox object to UIControl class type object
-    //this is called upcasting
-    //here type can be Object ex - public static void show(UIControl control){...
     public static void show(Object control){
-        //call toString() method of TextBox class
-        System.out.println(control);//output: ""
+        //downcasting
+        var textBox = (TextBox)control;
+        textBox.setText("Hello World");
+
+        System.out.println(control);//output: "Hello World"
     }
 }
