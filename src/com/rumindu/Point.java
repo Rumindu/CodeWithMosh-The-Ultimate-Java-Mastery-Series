0fbf37058e0b6c3ecf4d1b4d91965ede81b26a87
Rumindu De Silva <108813657+Rumindu@users.noreply.github.com>
2024-07-18 14:25:38 +0530

    IntelliJ generated equals() and hashCode()

--- src/com/rumindu/Point.java
+++ src/com/rumindu/Point.java
@@ -1,8 +1,8 @@
+//Point.java
 package com.rumindu;
 
 import java.util.Objects;
 
-//Point.java
 public class Point {
     private int x;
     private int y;
@@ -11,23 +11,35 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-
+    // generated from intelliJ
     @Override
-    public boolean equals(Object obj) {
-        //if the obj is equal to the current instance of the class
-        if(this == obj)
-            return true;
-        if(!(obj instanceof Point))
-            return false;
-        var other = (Point) obj;
-        return x == other.x && y == other.y;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
     }
 
     @Override
     public int hashCode() {
-        //`Objects` class is imported from java.util package
-        //multiple values can pass to hash() and -
-        //-it returns hash value according to those
         return Objects.hash(x, y);
     }
+    //    @Override
+//    public boolean equals(Object obj) {
+//        //if the obj is equal to the current instance of the class
+//        if(this == obj)
+//            return true;
+//        if(!(obj instanceof Point))
+//            return false;
+//        var other = (Point) obj;
+//        return x == other.x && y == other.y;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        //`Objects` class is imported from java.util package
+//        //multiple values can pass to hash() and -
+//        //-it returns hash value according to those
+//        return Objects.hash(x, y);
+//    }
 }
